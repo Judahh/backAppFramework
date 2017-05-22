c59e8c887de469b228508ff0567744d549db1211
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2017-05-22 08:55:27 -0300

    update

--- backApp/backApp.ts
+++ backApp/backApp.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 // import * as express from 'express';
-import Page from './page/Page';
+import Page from './page/page';
 import {Express, RequestHandler, Router, Request, Response, NextFunction} from 'express';
 import * as express from 'express';
 import * as logger from 'morgan';
