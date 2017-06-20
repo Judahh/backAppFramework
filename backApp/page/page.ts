067df7ca9a1c9c33521dff890c9c63b0e3b37c2f
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2017-06-20 13:52:24 -0300

    up

--- backApp/page/page.ts
+++ backApp/page/page.ts
@@ -1,6 +1,7 @@
 import {Router, Request, Response, NextFunction} from 'express';
 import * as logger from 'morgan';
 import * as path from 'path';
+import * as childProcess from 'child_process';
 
 export class Page {
   private router: Router
@@ -31,6 +32,35 @@ export class Page {
   }
 
   /**
+   * GET all Heroes.
+   */
+  public refresh(request: Request, response: Response, nextFunction: NextFunction) {
+    console.log(request.body.pusher.name + " pushed to " + request.body.repository.name);
+    console.log("Pulling code from Github...");
+
+    // reset any changes that have been made locally
+    childProcess.exec('git -C /home/projects/backAppFramework reset --hard', this.execCallback);
+
+    // and ditch any files that have been added locally too
+    childProcess.exec('git -C /home/projects/backAppFramework clean -df', this.execCallback);
+
+    // now pull down the latest
+    childProcess.exec('git -C /home/projects/backAppFramework pull -f', this.execCallback);
+
+    // and npm install with --production
+    childProcess.exec('npm -C /home/projects/backAppFramework install --production', this.execCallback);
+
+    // and run tsc
+    childProcess.exec('tsc', this.execCallback);
+
+  }
+
+  public execCallback(err, stdout, stderr) {
+    if(stdout) console.log(stdout);
+    if(stderr) console.log(stderr);
+  }
+
+  /**
    * Take each handler, and attach to one of the Expresponses.Router's
    * endpoints.
    */
@@ -39,6 +69,7 @@ export class Page {
     this.router.get('/:id', this.getOne);
     this.router.post('/', this.getPage);
     this.router.post('/:id', this.getOne);
+    this.router.post('/refresh', this.refresh);
   }
 
 }
