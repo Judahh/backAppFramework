b6240d04031d49c0f66b3e9fb6cf8e971c8b19a1
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2017-06-20 17:25:07 -0300

    asd

--- backApp/backApp.ts
+++ backApp/backApp.ts
@@ -57,7 +57,7 @@ export class BackApp {
 
   public run() {
       // this.express.listen(this.port);  
-      console.info('ts-express:server');
+      console.info('ts-express:server'); 
       this.server = http.createServer(this.express);
       this.server.listen(this.port);
       this.server.on('error', () => this.onError);
