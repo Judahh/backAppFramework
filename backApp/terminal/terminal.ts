659cde13295b3965713a3d7183bd91ec844fc0f0
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2017-06-22 13:48:37 -0300

    update

--- backApp/terminal/terminal.ts
+++ backApp/terminal/terminal.ts
@@ -137,7 +137,7 @@ export class Terminal {
 
     // and npm install with --production
     // childProcess.exec('sudo npm install', Terminal.install);
-    process.exit();
+    // process.exit();
     // and run tsc
     // childProcess.exec('sudo tsc', Page.execCallback);
   }
