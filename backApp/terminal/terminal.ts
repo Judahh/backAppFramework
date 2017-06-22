36aead0bd6bac9e4fc5a21cb531254743604c4b1
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2017-06-22 09:04:04 -0300

    asd

--- backApp/terminal/terminal.ts
+++ backApp/terminal/terminal.ts
@@ -27,7 +27,7 @@ export class Terminal {
     };
     var options = {
       method: 'get',
-      url: 'http://127.0.0.1:4040/api/tunnels',
+      url: 'http://localhost:4040/api/tunnels',
       headers: {
         'Content-Type': 'application/json.',
         'User-Agent': 'request'
