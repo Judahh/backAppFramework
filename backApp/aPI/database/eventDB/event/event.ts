a83d231854f04b8828d774be5c6af0f827837b09
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2017-06-23 08:59:07 -0300

    update

--- backApp/aPI/database/eventDB/event/event.ts
+++ backApp/aPI/database/eventDB/event/event.ts
@@ -1,3 +1,46 @@
-export class Event{
+import {Operation} from "./operation";
+export class Event {
+    private timestamp: string;
+    private operation: Operation;
+    private message: string;
+    private content;
 
+    constructor(operation: Operation, message: string, content) {
+        this.timestamp = this.getCurrentTimestamp();
+        this.operation = operation;
+        this.message = message;
+        this.content = content;
+    }
+
+    private getCurrentTimestamp() {
+        var d = new Date()
+        var dash = "-"
+        var colon = ":"
+        return d.getFullYear() + dash +
+            this.pad(d.getMonth() + 1) + dash +
+            this.pad(d.getDate()) + " " +
+            this.pad(d.getHours()) + colon +
+            this.pad(d.getMinutes()) + colon +
+            this.pad(d.getSeconds())
+    }
+
+    private pad(n) {
+        return n < 10 ? "0" + n : n
+    }
+
+    public getTimestamp(){
+        return this.timestamp;
+    }
+
+    public getOperation(){
+        return this.operation;
+    }
+
+    public getMessage(){
+        return this.message;
+    }
+
+    public getContent(){
+        return this.content;
+    }
 }
--- backApp/aPI/webhook/webhook.ts
+++ backApp/aPI/webhook/webhook.ts
@@ -1,4 +1,6 @@
 import { EventDB } from "./../database/eventDB/eventDB";
+import { Event } from "./../database/eventDB/event/event";
+import {Operation} from "./../database/eventDB/event/operation";
 import * as MongoDB from "mongodb";
 
 export class Webhook {
@@ -6,8 +8,13 @@ export class Webhook {
   private link: string;
   private data;
   private token;
+
   private addOptions;
+  private readOptions;
+  private readAllOptions;
+  private updateOptions;
   private deleteOptions;
+  
   private eventDB: EventDB;
 
   constructor(link: string) {
@@ -40,6 +47,42 @@ export class Webhook {
       }
     };
 
+    this.readOptions = {
+      method: 'get',
+      json: true,
+      url: 'https://api.github.com/repos/Judahh/backAppFramework/hooks/' + this.id,
+      headers: {
+        'Authorization': 'token ' + this.token,
+        'Content-Length': Buffer.byteLength(stringData, 'utf8'),
+        'Content-Type': 'application/json.',
+        'User-Agent': 'request'
+      }
+    };
+
+    this.readAllOptions = {
+      method: 'get',
+      json: true,
+      url: 'https://api.github.com/repos/Judahh/backAppFramework/hooks',
+      headers: {
+        'Authorization': 'token ' + this.token,
+        'Content-Length': Buffer.byteLength(stringData, 'utf8'),
+        'Content-Type': 'application/json.',
+        'User-Agent': 'request'
+      }
+    };
+
+    this.updateOptions = {
+      method: 'patch',
+      json: true,
+      url: 'https://api.github.com/repos/Judahh/backAppFramework/hooks/' + this.id,
+      headers: {
+        'Authorization': 'token ' + this.token,
+        'Content-Length': Buffer.byteLength(stringData, 'utf8'),
+        'Content-Type': 'application/json.',
+        'User-Agent': 'request'
+      }
+    };
+
     this.deleteOptions = {
       method: 'delete',
       json: true,
@@ -53,6 +96,10 @@ export class Webhook {
     };
 
     this.eventDB = new EventDB();
+  }
+
+  public setId(id: number) {
+    this.id = id;
   }
 
   public getId() {
@@ -71,23 +118,7 @@ export class Webhook {
     return this.token;
   }
 
-  public getAddOptions() {
-    var self=this;
-    this.eventDB.connect(this.addEvent);
-    return this.addOptions;
-  }
-
-  public addEvent = (error, db: MongoDB.Db) =>{
-    if (error) {
-      console.error(error);
-    }
-    console.log("ADD EVENT");
-    var events = db.collection('events');
-
-    events.insert(this.addOptions, this.addEventResult);
-  }
-
-  public addEventResult = (error, result) =>{
+  public eventResult = (error, result) => {
     console.log("RESULT EVENT");
     if (error) {
       console.error(error);
@@ -96,11 +127,113 @@ export class Webhook {
     }
   }
 
+  public getAddOptions() {
+    this.eventDB.connect(this.addEvent);
+    return this.addOptions;
+  }
+
+  public addEvent = (error, db: MongoDB.Db) => {
+    if (error) {
+      console.error(error);
+    }
+    console.log("ADD EVENT");
+    var events = db.collection('events');
+
+    var addContent = {
+      id:this.id,
+      link:this.link
+    };
+
+    var event = new Event(Operation.add,"Webhook",addContent);
+
+    events.insert(event, this.eventResult);
+  }
+
   public getDeleteOptions() {
+    this.eventDB.connect(this.deleteEvent);
     return this.deleteOptions;
   }
 
-  public setId(id: number) {
-    this.id = id;
+  public deleteEvent = (error, db: MongoDB.Db) => {
+    if (error) {
+      console.error(error);
+    }
+    console.log("DELETE EVENT");
+    var events = db.collection('events');
+
+    var addContent = {
+      id:this.id,
+      link:this.link
+    };
+
+    var event = new Event(Operation.delete,"Webhook",addContent);
+
+    events.insert(event, this.eventResult);
+  }
+
+  public getCorrectOptions() {
+    this.eventDB.connect(this.correctEvent);
+    return this.updateOptions;
+  }
+
+  public correctEvent = (error, db: MongoDB.Db) => {
+    if (error) {
+      console.error(error);
+    }
+    console.log("CORRECT EVENT");
+    var events = db.collection('events');
+
+    var addContent = {
+      id:this.id,
+      link:this.link
+    };
+
+    var event = new Event(Operation.correct,"Webhook",addContent);
+
+    events.insert(event, this.eventResult);
+  }
+
+  public getUpdateOptions() {
+    this.eventDB.connect(this.updateEvent);
+    return this.updateOptions;
+  }
+
+  public updateEvent = (error, db: MongoDB.Db) => {
+    if (error) {
+      console.error(error);
+    }
+    console.log("UPDATE EVENT");
+    var events = db.collection('events');
+
+    var addContent = {
+      id:this.id,
+      link:this.link
+    };
+
+    var event = new Event(Operation.update,"Webhook",addContent);
+
+    events.insert(event, this.eventResult);
+  }
+
+  public getNonexistentOptions() {
+    this.eventDB.connect(this.nonexistentEvent);
+    return this.deleteOptions;
+  }
+
+  public nonexistentEvent = (error, db: MongoDB.Db) => {
+    if (error) {
+      console.error(error);
+    }
+    console.log("NONEXISTENT EVENT");
+    var events = db.collection('events');
+
+    var addContent = {
+      id:this.id,
+      link:this.link
+    };
+
+    var event = new Event(Operation.nonexistent,"Webhook",addContent);
+
+    events.insert(event, this.eventResult);
   }
 }
