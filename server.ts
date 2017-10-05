0d31bd43c6fcdfdb01494db9b219202563878e0d
Judahh <judahholanda7@gmail.com>
2017-10-05 12:13:11 -0300

    update

--- server.ts
+++ server.ts
@@ -1,15 +1,9 @@
 import * as express from 'express';
-import {ApiConfiguration} from './api/apiConfiguration';
+import { Util } from 'basicutil';
+import { ApiConfiguration } from './api/apiConfiguration';
 require('dotenv').config();
 
-function normalizePort(val: number|string): number|string|boolean {
-  let port: number = (typeof val === 'string') ? parseInt(val, 10) : val;
-  if (isNaN(port)) return val;
-  else if (port >= 0) return port;
-  else return false;
-}
-
-let backApp = new ApiConfiguration(express(), normalizePort(process.env.PORT || 3000));
+let backApp = new ApiConfiguration(express(), Util.normalizePort(process.env.PORT || 3000));
 backApp.run();
 
 
