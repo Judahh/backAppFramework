971fd20b958195d3eded9e11a79cf21f14f32531
Judahh <judahholanda7@gmail.com>
2017-11-01 13:09:10 -0300

    update

--- api/electron/electron.ts
+++ api/electron/electron.ts
@@ -5,8 +5,63 @@ import * as url from 'url';
 export class Electron {
     private mainWindow: any;
 
-    constructor() {
-        if(app!=undefined){
+    private touch;
+    private touchSimulate;
+    private frame;
+    private kiosk;
+    private node;
+    private development;
+    private width: number;
+    private height: number;
+    private fullscreen;
+    private useContentSize
+    private autoHideMenuBar
+    private title: string;
+    private console;
+    private url: string;
+    private zoom: number;
+    private overlayScrollbars;
+    private icon: string;
+
+    constructor(touch?, touchSimulate?, frame?, kiosk?, node?, width?, height?, fullscreen?, useContentSize?,
+        autoHideMenuBar?, title?, consoleVar?, url?, zoom?, overlayScrollbars?, development?, icon?) {
+        if (app != undefined) {
+            this.touch = touch === '1' ? 1 : 0;
+            this.touchSimulate = touchSimulate === '1' ? 1 : 0;
+            this.frame = frame === '1' ? 1 : 0;
+            this.kiosk = kiosk === '1' ? 1 : 0;
+            this.node = node === '1' ? 1 : 0;
+            this.width = parseInt(width || 1920, 10);
+            this.height = parseInt(height || 1080, 10);
+            this.fullscreen = fullscreen === '1' ? 1 : 0;
+            this.useContentSize = useContentSize === '1' ? 1 : 0;
+            this.autoHideMenuBar = autoHideMenuBar === '1' ? 1 : 0;
+            this.title = title;
+            this.console = consoleVar === '1' ? 1 : 0;
+            this.url = url;
+            this.zoom = parseFloat(zoom || 1.0);
+            this.overlayScrollbars = overlayScrollbars === '1' ? 1 : 0;
+            this.development = development;
+            this.icon = icon;
+
+            // enable touch events if your device supports them
+            if (this.touch) {
+                app.commandLine.appendSwitch('--touch-devices');
+            }
+            // simulate touch events - might be useful for touchscreen with partial driver support
+            if (this.touchSimulate) {
+                app.commandLine.appendSwitch('--simulate-touch-screen-with-mouse');
+            }
+
+            if (this.development === 'development') {
+                console.log('Running in development mode');
+                this.height = 600;
+                this.width = 800;
+                this.kiosk = 0;
+                this.console = 1;
+                this.frame = 1;
+            }
+
             this.init();
         }
     }
@@ -14,23 +69,34 @@ export class Electron {
     private createWindow() {
         // Create the browser window.
         //let appIcon = new Tray('../favicon.png');
-        let appIcon = nativeImage.createFromPath(__dirname + '/favicon.png');
+        let appIcon = nativeImage.createFromPath(path.resolve(this.icon));
+        // console.log(path.resolve(this.icon));
 
         //   mainWindow = new BrowserWindow({width: 800, height: 600})
 
         this.mainWindow = new BrowserWindow({
-            fullscreen: false,
-            frame: true,
-            useContentSize: true,
-            autoHideMenuBar: true,
-            //icon: __dirname + '/favicon.png'
-            icon: appIcon
+            fullscreen: this.fullscreen,
+            frame: !!(this.frame),
+            useContentSize: this.useContentSize,
+            autoHideMenuBar: this.autoHideMenuBar,
+            icon: appIcon,
+            width: this.width,
+            height: this.height,
+            title: this.title,
+            kiosk: !!(this.kiosk),
+            webPreferences: {
+                nodeIntegration: !!(this.node),
+                zoomFactor: this.zoom//,
+                //   overlayScrollbars: !!(this.overlayScrollbars)
+            }
         });
 
-        this.mainWindow.loadURL('http://localhost:3000/');//TODO: remover 3000
+        this.mainWindow.loadURL(this.url);//TODO: remover 3000
 
         // Open the DevTools.
-        //win.webContents.openDevTools()
+        if (this.console) {
+            this.mainWindow.webContents.openDevTools()
+        }
 
         // Emitted when the window is closed.
         this.mainWindow.on('closed', () => {
@@ -58,10 +124,11 @@ export class Electron {
     }
 
     private init() {
-        app.on('ready', this.createWindow);
+        let _self = this;
+        app.on('ready', () => { _self.createWindow() });
         // Quit when all windows are closed.
-        app.on('window-all-closed', this.quit);
+        app.on('window-all-closed', () => { _self.quit() });
 
-        app.on('activate', this.activate);
+        app.on('activate', () => { _self.activate() });
     }
 }
