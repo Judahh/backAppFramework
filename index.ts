5126211de6037757c25217fd8b539d8d996a3cf6
Judahh <judahholanda7@gmail.com>
2017-12-01 13:21:09 -0300

    update

--- index.ts
+++ index.ts
@@ -5,8 +5,9 @@ import { ApiConfiguration } from './api/apiConfiguration';
 import { BasicApi } from './api/basicApi';
 import { BasicAppHandler } from './api/appHandler/basicAppHandler';
 import { BasicExternalHandler } from './api/externalHandler/basicExternalHandler';
+import { BasicSocket } from './api/socket/basicSocket';
 import { Router, Request, Response, NextFunction } from 'express';
 import { Electron } from './api/electron/electron';
 import { Server } from './server';
 
-export { Server, Router, Request, Response, NextFunction, Electron, ApiConfiguration, BasicApi, BasicAppHandler, BasicExternalHandler, express, path }
+export { Server, Router, Request, Response, NextFunction, Electron, ApiConfiguration, BasicApi, BasicAppHandler, BasicExternalHandler, BasicSocket, express, path }
