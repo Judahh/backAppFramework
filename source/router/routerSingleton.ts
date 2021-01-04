ac80bfaf2d8b1f53f33558fa3a37ab178991ccb1
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2021-01-04 11:18:30 -0300

    add more status and errors and fix response mock

--- source/router/routerSingleton.ts
+++ source/router/routerSingleton.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { Router } from 'express';
