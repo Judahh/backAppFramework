475c170cbd25df6218c88a70ac17f2a20692e465
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2021-05-06 16:59:16 -0300

    init

--- source/adapter/controllerConnectAdapter.ts
+++
@@ -1,4 +0,0 @@
-/* eslint-disable no-unused-vars */
-export default interface ControllerConnectAdapter {
-  connect(request, response): Promise<Response>;
-}
--- test/integration/testController.ts
+++
@@ -1,7 +0,0 @@
-import { BaseController } from '../../source';
-
-export default class TestController extends BaseController {
-  protected element = 'test';
-
-  protected elements = 'tests';
-}
